46888322e94a39908dc2edaf006a313a19852649
Nhan Phan <kippulainenko@gmail.com>
2022-08-21 16:56:09 +0300

    change icons for the buttons

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,6 +5,7 @@ import Card from "../components/ui/Card";
 import InstructionText from "../components/ui/InstructionText";
 import PrimaryButton from "../components/ui/PrimaryButton";
 import Title from "../components/ui/Title";
+import { Ionicons } from "@expo/vector-icons";
 
 function generateRandomBetween(min, max, exclude) {
   const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -61,10 +62,14 @@ const GameScreen = ({userNumber, onGameOver}) => {
           <InstructionText style={styles.instructionText}>Higher or Lower</InstructionText>
           <View style={styles.buttonsContainer}>
             <View style={styles.buttonContainer}>
-              <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
+              <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>
+                <Ionicons name="md-remove" size={24} color="white" />
+              </PrimaryButton>
             </View>
             <View style={styles.buttonContainer}>
-              <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
+              <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>
+                <Ionicons name="md-add" size={24} color="white" />
+              </PrimaryButton>
             </View>
           </View>
         </Card>
